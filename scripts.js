f935d27a4aadb435bfed8697bb8465f78b7a9707
rupesh-mutyala <rupesh.mutyala@cogoport.com>
2023-08-22 18:34:30 +0530

    minor changes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,24 @@ const onClick = (event) => {
     const button = event.target.closest('BUTTON');  // for getting the nearest button
 
     if (button) {
-        console.log('buttonid', button.getAttribute('id')); // logging the button that is clicked
+        const alertFunction = () =>  window.alert(`You clicked on button with id ${button.getAttribute('id')}. we are using a property called "Event Delegation" which improves Memory Efficiency, Performance and will make the code simpler`);
+
+        const onClickButton1 = () => {
+            // write your code here
+            alertFunction()
+        }
+
+        const onClickButton2 = () => {
+            // write your code here
+            alertFunction()
+        }
+
+        const funcMapping = {
+			button1  : onClickButton1,
+			button2 : onClickButton2,
+		};
+
+        funcMapping[button.getAttribute('id')]();
 
         event.stopPropagation();
         event.preventDefault();
